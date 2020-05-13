09bcfcab4b26c1334e2e29542a96faf14c0ae84d
Takenoko <takenoko.tech@gmail.com>
2020-05-13 16:03:56 +0900

    add bubble.

---
+++ Assets/Project/Scripts/Runtime/effect/BubbleMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts.Runtime.effect
+{
+    public class BubbleMove : MonoBehaviour
+    {
+        [SerializeField] private bool emit = false;
+        [SerializeField] private GameObject particle;
+        
+        private MeshRenderer mesh = null;
+        private GameObject cam = null;
+        private float randFloat = 0;
+        
+        void Start()
+        {
+            mesh = GetComponent<MeshRenderer>();
+            if (Camera.main != null) cam = Camera.main.gameObject;
+            randFloat = Random.Range(-10.0f, 10.0f);
+        }
+
+        void Update()
+        {
+            if (cam != null)
+                transform.LookAt(cam.transform);
+
+            var pos = transform.position;
+            transform.position =
+                new Vector3(pos.x, pos.y + 0.001F * Mathf.Sin(Time.frameCount * 0.1F + randFloat), pos.z);
+
+            if (emit)
+            {
+                mesh.enabled = false;
+                particle.SetActive(true);
+            }
+            else
+            {
+                mesh.enabled = true;
+                particle.SetActive(false);                
+            }
+        }
+    }
+}
