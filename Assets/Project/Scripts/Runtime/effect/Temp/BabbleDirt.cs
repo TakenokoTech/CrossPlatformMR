f4b05ad08be901f4130070e635c039cdd704cb02
Takenoko <takenoko.tech@gmail.com>
2020-05-16 17:35:48 +0900

    fix babble.

--- Assets/Project/Scripts/Runtime/effect/Temp/BabbleDirt.cs
+++ Assets/Project/Scripts/Runtime/effect/Temp/BabbleDirt.cs
@@ -6,25 +6,30 @@ namespace Project.Scripts.Runtime.effect.Temp
     {
         [SerializeField] private Bubble.Bubble bubble;
         [SerializeField] private Dirt.Dirt dirt;
+        [SerializeField] internal bool emit = false;
         
         private Rigidbody rigid;
         private Vector3 pos;
+        private Quaternion rot;
 
         // Start is called before the first frame update
         void Start()
         {
             rigid = dirt.gameObject.GetComponent<Rigidbody>();
             pos = rigid.gameObject.transform.localPosition;
+            rot = rigid.gameObject.transform.localRotation;
         }
 
         // Update is called once per frame
         void Update()
         {
             rigid.useGravity = bubble.emit;
+            bubble.emit = emit;
 
             if (!bubble.emit)
             {
                 rigid.gameObject.transform.localPosition = pos;
+                rigid.gameObject.transform.localRotation = rot;
             }
         }
     }
